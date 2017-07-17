e7819daa77bd5924d8887cfcb13daa1d8c0bf90c
DarcInc <darcinc@protonmail.com>
2017-07-16 21:40:01 -0400

    Adding basic keystore management

---
+++ keystore.go
@@ -0,0 +1,157 @@
+package repository
+
+import (
+	"path"
+
+	"os"
+
+	"crypto/rsa"
+
+	"crypto/x509"
+
+	"encoding/json"
+
+	"github.com/spf13/afero"
+)
+
+func defaultDirectory() string {
+	return path.Join(os.Getenv("HOME"), ".repkey")
+}
+
+func namedKeystoreFile(location string) string {
+	return path.Join(defaultDirectory(), location+".sqlite")
+}
+
+func keystoreExists(fs afero.Fs, location string) (bool, error) {
+	var dir string
+	var fullpath string
+
+	if path.IsAbs(location) {
+		dir = path.Dir(location)
+		fullpath = location
+	} else {
+		dir = defaultDirectory()
+		fullpath = namedKeystoreFile(location)
+	}
+
+	ok, err := afero.DirExists(fs, dir)
+	if err != nil {
+		return false, err
+	}
+
+	if !ok {
+		return false, nil
+	}
+
+	ok, err = afero.Exists(fs, fullpath)
+	if err != nil {
+		return false, err
+	}
+
+	return ok, nil
+}
+
+// Keystore is the collection fo keys
+type Keystore struct {
+	PrivateKeys map[string][]byte
+	PublicKeys  map[string][]byte
+}
+
+// CreateKeystore creates a new keystore or fails if one exist
+func CreateKeystore(fs afero.Fs, name string) (*Keystore, error) {
+	result := &Keystore{}
+	result.PrivateKeys = make(map[string][]byte)
+	result.PublicKeys = make(map[string][]byte)
+
+	file, err := fs.OpenFile(namedKeystoreFile(name), os.O_CREATE|os.O_EXCL|os.O_RDWR, 0600)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	return result, nil
+}
+
+// AddPrivateKey adds a private key to the keystore
+func (k *Keystore) AddPrivateKey(name string, key *rsa.PrivateKey) {
+	bytes := x509.MarshalPKCS1PrivateKey(key)
+	k.PrivateKeys[name] = bytes
+}
+
+// FindPrivateKey finds a private key from the keystore
+func (k *Keystore) FindPrivateKey(name string) (*rsa.PrivateKey, bool) {
+	bytes, ok := k.PrivateKeys[name]
+	if !ok {
+		return nil, ok
+	}
+
+	result, err := x509.ParsePKCS1PrivateKey(bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	return result, ok
+}
+
+// FindPublicKey is returned the public key for a given name
+func (k *Keystore) FindPublicKey(name string) (*rsa.PublicKey, bool) {
+	bytes, ok := k.PrivateKeys[name]
+	if !ok {
+		bytes, ok = k.PublicKeys[name]
+		if !ok {
+			return nil, false
+		}
+		result, err := x509.ParsePKIXPublicKey(bytes)
+		if err != nil {
+			panic(err)
+		}
+
+		return result.(*rsa.PublicKey), ok
+	}
+	result, err := x509.ParsePKCS1PrivateKey(bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	return &result.PublicKey, ok
+}
+
+// AddPublicKey to keystore
+func (k *Keystore) AddPublicKey(name string, key *rsa.PublicKey) {
+	bytes, err := x509.MarshalPKIXPublicKey(key)
+	if err != nil {
+		panic(err)
+	}
+
+	k.PublicKeys[name] = bytes
+}
+
+// Save saves a keystore to a file
+func (k *Keystore) Save(file afero.File) error {
+	encoder := json.NewEncoder(file)
+	return encoder.Encode(k)
+}
+
+// OpenKeystore opens a keystore pointed to by a file
+func OpenKeystore(file afero.File) (*Keystore, error) {
+	decoder := json.NewDecoder(file)
+	keystore := &Keystore{
+		PrivateKeys: make(map[string][]byte),
+		PublicKeys:  make(map[string][]byte),
+	}
+	err := decoder.Decode(keystore)
+	if err != nil {
+		return nil, err
+	}
+
+	return keystore, nil
+}
+
+// KeystorePath returns the path to a keystore
+func KeystorePath(name string) string {
+	if path.IsAbs(name) {
+		return name
+	}
+
+	return namedKeystoreFile(name)
+}
---
+++ keystore_test.go
@@ -0,0 +1,208 @@
+package repository
+
+import (
+	"fmt"
+	"testing"
+
+	"os"
+
+	"crypto/rand"
+	"crypto/rsa"
+
+	"crypto/x509"
+
+	"github.com/spf13/afero"
+)
+
+func createTestFs() (afero.Fs, error) {
+	appfs := afero.NewMemMapFs()
+
+	userHome := os.Getenv("HOME")
+	dir := fmt.Sprintf("%s/.repkey", userHome)
+	filename := "keystore.sqlite"
+
+	appfs.MkdirAll(dir, 0700)
+	file, err := appfs.Create(fmt.Sprintf("%s/%s", dir, filename))
+	if err != nil {
+		return nil, err
+	}
+
+	file.Close()
+	return appfs, nil
+}
+
+func TestAbsolutePathKeystoreExists(t *testing.T) {
+	appfs, err := createTestFs()
+	if err != nil {
+		t.Fatalf("TestAbsolutePathKeystoreExists - Failed to create test filesystem: %v", err)
+	}
+
+	ok, err := keystoreExists(appfs, fmt.Sprintf("%s/.repkey/keystore.sqlite", os.Getenv("HOME")))
+	if err != nil {
+		t.Fatalf("TestAbsolutePathKeystoreExists - Failed to check existence of keystore: %v", err)
+	}
+
+	if !ok {
+		t.Errorf("TestAbsolutePathKeystoreExists - Expected keystore to exist")
+	}
+}
+
+func TestNamedKeystoreExists(t *testing.T) {
+	appfs, err := createTestFs()
+	if err != nil {
+		t.Fatalf("TestNamedKeystoreExists - Failed to create test filesystem: %v", err)
+	}
+
+	ok, err := keystoreExists(appfs, "keystore")
+	if err != nil {
+		t.Fatalf("TestNamedKeystoreExists - Failed to check existence of keystore: %v", err)
+	}
+
+	if !ok {
+		t.Error("TestNamedKeystoreExists - Expected keystore to exist")
+	}
+}
+
+func TestCreateNamedKeystore(t *testing.T) {
+	appfs, err := createTestFs()
+	if err != nil {
+		t.Fatalf("TestCreateNamedKeystore - Failed to create test filesystem: %v", err)
+	}
+
+	keystore, err := CreateKeystore(appfs, "foo")
+	if err != nil {
+		t.Fatalf("TestCreateNamedKeystore - Failed to create keystore: %v", err)
+	}
+	if keystore == nil {
+		t.Fatalf("TestCreateNamedKeystore - Keystore is nil")
+	}
+
+	ok, err := keystoreExists(appfs, "foo")
+	if err != nil {
+		t.Fatalf("TestCreateNamedKeystore - Failed to check the existence of keystore: %v", err)
+	}
+
+	if !ok {
+		t.Error("TestCreateNamedKeystore - Expected keystore 'foo' to exist")
+	}
+}
+
+func TestAddPublicPrivateKey(t *testing.T) {
+	appfs, err := createTestFs()
+	if err != nil {
+		t.Fatalf("TestCreateNamedKeystore - Failed to create test filesystem: %v", err)
+	}
+
+	keystore, err := CreateKeystore(appfs, "foo")
+	if err != nil {
+		t.Fatalf("TestCreateNamedKeystore - Failed to create test filesystem: %v", err)
+	}
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 1024)
+	keystore.AddPrivateKey("foo", privateKey)
+
+	foundPrivateKey, ok := keystore.FindPrivateKey("foo")
+	if !ok {
+		t.Fatalf("TestCreateNamedKeystore - Failed to find the key just inserted")
+	}
+
+	bytes1 := x509.MarshalPKCS1PrivateKey(privateKey)
+	bytes2 := x509.MarshalPKCS1PrivateKey(foundPrivateKey)
+
+	for i := range bytes1 {
+		if bytes1[i] != bytes2[i] {
+			t.Fatalf("TestCreateNamedKeystore - Mismatch")
+		}
+	}
+
+	foundPublicKey, ok := keystore.FindPublicKey("foo")
+	if !ok {
+		t.Fatalf("TestCreateNamedKeystore - Failed to find the public key for the key just inserted")
+	}
+
+	bytes1, err = x509.MarshalPKIXPublicKey(foundPublicKey)
+	if err != nil {
+		t.Fatalf("TestCreateNamedKeystore - Failed to marshal found public key: %v", err)
+	}
+	bytes2, err = x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		t.Fatalf("TestCreateNamedKeystore - Failed to marshal public key: %v", err)
+	}
+
+	for i := range bytes1 {
+		if bytes1[i] != bytes2[i] {
+			t.Fatalf("TestCreateNamedKeystore - Mismatch")
+		}
+	}
+}
+
+func TestAddPublicKey(t *testing.T) {
+	appfs, err := createTestFs()
+	if err != nil {
+		t.Fatalf("TestAddPublicKey - Failed to create test filesystem: %v", err)
+	}
+
+	keystore, err := CreateKeystore(appfs, "foo")
+	if err != nil {
+		t.Fatalf("TestAddPublicKey - Failed to create test filesystem: %v", err)
+	}
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		t.Fatalf("TestAddPublicKey - Failed to generate key")
+	}
+
+	keystore.AddPublicKey("foo", &privateKey.PublicKey)
+
+	foundKey, ok := keystore.FindPublicKey("foo")
+	if !ok {
+		t.Fatalf("TestAddPublicKey - Failed to find the key just added")
+	}
+
+	bytes1, err := x509.MarshalPKIXPublicKey(foundKey)
+	if err != nil {
+		t.Fatalf("TestAddPublicKey - Failed to marshal found public key: %v", err)
+	}
+	bytes2, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		t.Fatalf("TestAddPublicKey - Failed to marshal public key: %v", err)
+	}
+
+	for i := range bytes1 {
+		if bytes1[i] != bytes2[i] {
+			t.Fatalf("TestAddPublicKey - Mismatch")
+		}
+	}
+}
+
+func TestSaveKeystore(t *testing.T) {
+	appfs, err := createTestFs()
+	if err != nil {
+		t.Fatalf("TestSaveKeystore - Failed to create test filesystem: %v", err)
+	}
+
+	keystore, err := CreateKeystore(appfs, "foo")
+	privateKey, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		t.Fatalf("TestSaveKeystore - Failed to generate key")
+	}
+
+	keystore.AddPrivateKey("bar", privateKey)
+	fullPath := KeystorePath("foo")
+	keyFile, err := appfs.OpenFile(fullPath, os.O_WRONLY, 0600)
+	if err != nil {
+		t.Fatalf("TestSaveKeystore - Failed to open save file: %v", err)
+	}
+
+	err = keystore.Save(keyFile)
+	keyFile.Close()
+
+	keyFile, err = appfs.OpenFile(fullPath, os.O_RDONLY, 0600)
+	keystore, err = OpenKeystore(keyFile)
+	defer keyFile.Close()
+
+	privateKey, ok := keystore.FindPrivateKey("bar")
+	if !ok {
+		t.Fatal("TestSaveKeystore - Failed to find private key")
+	}
+}
